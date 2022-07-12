049cf0d3f13d24b69f0f40e81ae808bf7b488cf7
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-07-12 11:24:25 +0200

    Edited 01_output

--- 01_output.php
+++ 01_output.php
@@ -1,3 +1,28 @@
 <?php
-    echo 'Hasellssoss damb sheeesh';
+    //echo 'Hasellssoss damb sheeesh ', 2, 10.5;
+
+    //print 123;
+
+    //print_r([1,2,3]);
+
+    //var_dump(true);
+
+    //var_export('Hello');
+
+
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo 'Post one'; ?></h1>
+</body>
+</html>
