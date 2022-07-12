3fba3e4f5e740dc2107cbff6627af927bd819434
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-07-12 11:37:14 +0200

    Added and edited 02_variables

---
+++ 02_variables.php
@@ -0,0 +1,28 @@
+<?php
+    $name = 'Brad'; //string
+    $age = 48; //int
+    $has_kids=true; //bool
+    $cash_on_hand=20.75; //float
+
+    //echo $age; //putput age
+    //var_dump($has_kids);
+    //var_dump($cash_on_hand);
+
+    //echo $name .' is '.$age.' years old.';
+    //echo "\n$name is $age years old.";
+
+    $x = 5+5;
+    /*var_dump($x);
+    echo 10-5;
+    echo 5*6;
+    echo 10/2;
+    echo 10%3;*/
+
+    define('HOST', 'localhost');
+    define('DB_NAME', 'dev_db');
+
+    echo HOST;
+    
+
+?>
+
